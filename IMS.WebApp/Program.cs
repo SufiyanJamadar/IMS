97d429bc432779bbc9384890931ab5a29f007278
Sufiyan <sufiyanajamadar@gmail.com>
2025-05-20 15:05:49 +0530

    Add project files.

---
+++ IMS.WebApp/Program.cs
@@ -0,0 +1,41 @@
+using IMS.Plugins.InMemory;
+using IMS.UseCases.Inventories;
+using IMS.UseCases.PluginInterfaces;
+using IMS.WebApp.Components;
+
+namespace IMS.WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorComponents();
+
+            builder.Services.AddSingleton<IInventoryRepository, InventoryRepository>();
+
+            builder.Services.AddTransient<IViewInventoriesByNameUseCase,ViewInventoriesByNameUseCase>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.MapRazorComponents<App>();
+
+            app.Run();
+        }
+    }
+}
